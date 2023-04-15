273524a4b528a8ce2e63a096abcd9d425be8beee
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-15 12:13:28 -0300

    config: install and config chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { defaultTheme } from '@/theme/defaultTheme'
+
+const { colors } = defaultTheme
+
+const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+    colors: {
+        highlight: "#FFBA08",
+
+        light: {
+            info: "#DADADA",
+            text: "#F5F8FA",
+            white: "#FFFFFF"
+        },
+
+        dark: {
+            info: "#FFFFFF",
+            text: "#47585B",
+            black: "#000000"
+        }
+    }
+}
