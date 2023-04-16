c1444a20ca9125f8f8762a4afd6aef06a29f39c3
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-15 22:21:37 -0300

    config: adjust continent types

--- src/@types/continent.d.ts
+++ src/@types/continent.d.ts
@@ -1,7 +1,20 @@
+interface Country {
+    country: string;
+    image: string;
+}
+
+interface AmountDetails {
+    countries: number
+    languages: number
+    cities: number
+}
+
 export interface Continent {
     id: number
     name: string
     image: string
     slug: string
     summary: string
+    countries: Country[]
+    amountDetails: AmountDetails
 }
