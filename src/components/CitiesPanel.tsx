56c1cb7e971be713e00621e1ebb8e6d69d55d923
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-16 15:52:50 -0300

    feat: create Cities Panel component

---
+++ src/components/CitiesPanel.tsx
@@ -0,0 +1,67 @@
+import { Box, Flex, Grid, Heading, Image, Stack, Text } from "@chakra-ui/react"
+
+interface Props {
+    title: string
+    countries: {
+        country: string;
+        city: string;
+        image: string;
+        flag: string;
+    }[]
+}
+
+export function CitiesPanel({ title, countries }: Props) {
+    return (
+        <Box maxW="1160px" mx="auto">
+            <Heading>{title}</Heading>
+            <Grid
+                gridTemplateColumns="repeat(4,1fr)"
+                gap="2.8125rem"
+                mt="5rem"
+            >
+                {countries.map(({ city, country, flag, image }) => (
+                    <Box key={city}>
+                        <Image
+                            src={image}
+                            alt=""
+                            maxW="256px"
+                            h="173px"
+                            borderTopLeftRadius="4px"
+                            borderTopRadius="4px"
+                        />
+                        <Flex
+                            justifyContent="space-between"
+                            alignItems="center"
+                        >
+                            <Box
+                                display="flex"
+                                flexDirection="column"
+                                gap="0.75rem"
+                            >
+                                <Heading
+                                    fontSize="1.25rem"
+                                    fontWeight="semibold"
+                                >
+                                    {country}
+                                </Heading>
+                                <Text
+                                    fontSize="1rem"
+                                    color="light.100"
+                                >
+                                    {city}
+                                </Text>
+                            </Box>
+                            <Box
+                                as="span"
+                                className={flag}
+                                w="30px"
+                                h="30px"
+                                borderRadius="999px"
+                            />
+                        </Flex>
+                    </Box>
+                ))}
+            </Grid>
+        </Box>
+    )
+}
