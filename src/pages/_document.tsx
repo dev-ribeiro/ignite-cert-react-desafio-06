3c4f1467cdac9e31815df92aaca42391668fc94b
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-16 15:54:46 -0300

    fix: import flag icons lib cdn in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
