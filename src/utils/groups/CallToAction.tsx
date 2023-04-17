2fc917ca1cd2ea03e7bcd8ad2c6185657bbe9bdc
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-17 14:00:17 -0300

    fix: adjust responsivity of CTA

---
+++ src/utils/groups/CallToAction.tsx
@@ -0,0 +1,23 @@
+import { Heading, VStack } from "@chakra-ui/react";
+
+export function CallToAction() {
+    return (
+        <VStack
+            my={["1.5rem", "3.25rem"]}
+            alignItems="center"
+        >
+            <Heading
+                fontSize={["1.25rem", "2.25rem"]}
+                fontWeight="medium"
+            >
+                Vamos nessa?
+            </Heading>
+            <Heading
+                fontSize={["1.25rem", "2.25rem"]}
+                fontWeight="medium"
+            >
+                Então escolha seu continente
+            </Heading>
+        </VStack>
+    )
+}
