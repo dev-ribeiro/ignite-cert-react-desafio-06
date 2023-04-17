41fc032ee55a5d27aaf214bca86656553f0c08cf
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-17 08:05:50 -0300

    feat: implement Header variant with icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,43 @@
-import { Container } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Image from 'next/image'
 import logoImage from '../assets/logo.png'
+import Link from 'next/link'
 
-export function Header() {
+interface Props {
+    hasBackIcon?: boolean
+}
+
+export function Header({ hasBackIcon }: Props) {
     return (
-        <Container
+        <Box
+            maxW="1160px"
+            w="100%"
+            mx="auto"
             p={7}
-            centerContent
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            position="relative"
         >
+            {hasBackIcon && (
+                <Box
+                    pl="5rem"
+                    position="absolute"
+                    left={0}
+                >
+                    <Link href="/">
+                        <span className="material-symbols-outlined">
+                            arrow_back_ios
+                        </span>
+                    </Link>
+                </Box>
+            )}
             <Image
                 src={logoImage}
                 width={184}
                 height={46}
                 alt='Imagem de logo escrita worldtrip'
             />
-        </Container>
+        </Box>
     )
 }
