fdb0663dcff362b8919eb58eb4fd1defdbdb3057
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-17 16:49:25 -0300

    fix: create country highlight component

--- src/components/CountryHighlight.tsx
+++ src/components/CountryHighlight.tsx
@@ -10,49 +10,49 @@ export function CountryHighlight({ cities, countries, languages }: Props) {
     return (
         <Flex
             alignItems="center"
-            justifyContent="center"
+            justifyContent={["space-between", "center"]}
             gap="2.625rem"
         >
-            <Stack textAlign="center">
+            <Stack textAlign={["start", "center"]}>
                 <Text
-                    fontSize="3rem"
+                    fontSize={["1.5rem", "3rem"]}
                     color="highlight"
                     fontWeight="semibold"
                 >
                     {countries}
                 </Text>
                 <Text
-                    fontSize="1.5rem"
+                    fontSize={["1.125rem", "1.5rem"]}
                     fontWeight="semibold"
                 >
                     países
                 </Text>
             </Stack>
-            <Stack textAlign="center">
+            <Stack textAlign={["start", "center"]}>
                 <Text
-                    fontSize="3rem"
+                    fontSize={["1.5rem", "3rem"]}
                     color="highlight"
                     fontWeight="semibold"
                 >
                     {languages}
                 </Text>
                 <Text
-                    fontSize="1.5rem"
+                    fontSize={["1.125rem", "1.5rem"]}
                     fontWeight="semibold"
                 >
                     línguas
                 </Text>
             </Stack>
-            <Stack textAlign="center">
+            <Stack textAlign={["start", "center"]}>
                 <Text
-                    fontSize="3rem"
+                    fontSize={["1.5rem", "3rem"]}
                     color="highlight"
                     fontWeight="semibold"
                 >
                     {cities}
                 </Text>
                 <Text
-                    fontSize="1.5rem"
+                    fontSize={["1.125rem", "1.5rem"]}
                     fontWeight="semibold"
                 >
                     cidades
